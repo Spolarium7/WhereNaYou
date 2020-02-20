2f171e4ce902df4751364bae54da08cb615ac141
Goshen Jimenez <goshenjimenez@gmail.com>
2020-02-20 17:59:06 +0800

    Database and Project Setup

---
+++ GoshenJimenez.WhereNaYou/GoshenJimenez.WhereNaYou.API/Infrastructure/Domain/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using GoshenJimenez.WhereNaYou.API.Infrastructure.Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.WhereNaYou.API.Infrastructure.Domain.Models
+{
+    public class User : BaseModel
+    {
+        public User()
+        {
+            this.LoginStatus = LoginStatus.Inactive;
+        }
+
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public int LoginRetries { get; set; }
+
+        public LoginStatus LoginStatus { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public long Latitude { get; set; }
+
+        public long Longitude { get; set; }
+    }
+}
